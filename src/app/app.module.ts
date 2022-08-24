c7453c3e132e6b571283b67362d143dfe417201b
oaymohsin <oaymohsin@gmail.com>
2022-08-24 13:42:49 +0500

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './Admin-module/admin-components/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+  
   ],
   imports: [
     BrowserModule,
